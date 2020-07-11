7fe3d1b7b309ce5264549698da337f53c9c2364f
Cloter Gonçalves Barbosa Junior <cjunior-partner@bettatecnologia.com.br>
2020-07-11 18:42:38 -0300

    Finished Challenge 07

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,31 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.forEach(uploadedFile => {
+      data.append('file', uploadedFile.file, uploadedFile.name);
+    });
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const filesProps = files.map(file => {
+      const fileProps: FileProps = {
+        file,
+        name: file.name,
+        readableSize: file.size.toString(),
+      };
+
+      return fileProps;
+    });
+
+    setUploadedFiles(filesProps);
   }
 
   return (
